00dc447e5dc73b3da1a341d7f5415d8699deddc5
Martin Weiser <martin.weiser@gmail.com>
2026-03-18 17:11:21 +0100

    feat: udpates styling, radix theme, light, dark

--- src/lib/customized-radix-theme-provider.tsx
+++ src/lib/customized-radix-theme-provider.tsx
@@ -1,5 +1,17 @@
 import { Theme, type ThemeProps } from '@radix-ui/themes'
+import { useSyncExternalStore } from 'react'
 
-export const RadixTheme = (props: ThemeProps) => (
-  <Theme {...props} accentColor="blue" radius="small" />
-)
+const mq = window.matchMedia('(prefers-color-scheme: dark)')
+
+const subscribe = (cb: () => void) => {
+  mq.addEventListener('change', cb)
+  return () => mq.removeEventListener('change', cb)
+}
+
+const getSnapshot = (): 'light' | 'dark' => (mq.matches ? 'dark' : 'light')
+
+export const RadixTheme = (props: ThemeProps) => {
+  const appearance = useSyncExternalStore(subscribe, getSnapshot)
+
+  return <Theme {...props} appearance={appearance} accentColor="blue" radius="small" />
+}
