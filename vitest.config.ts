da2d9edad093e020bf93145f4632f59d86f7fed0
Martin Weiser <martin.weiser@gmail.com>
2026-04-08 18:43:02 +0200

    chore: formal vitest scss preprocessor declaration

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,6 +20,13 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: ['./src'],
+      },
+    },
+  },
   esbuild: {
     jsx: 'automatic',
     jsxImportSource: 'react',
